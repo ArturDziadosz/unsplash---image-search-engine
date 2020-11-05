60955a30c96817a9bf094cdc98bcdc169aff4943
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-11-05 11:40:56 +0100

    Added Form component with passing input value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
+import { useState } from 'react';
 import './App.scss';
+import Form from './components/Form';
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(searchValue);
+  }
+
   return (
     <div>
-      Initial
+      <Form 
+        searchValue={searchValue} 
+        handleSearchValueChange={setSearchValue}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
