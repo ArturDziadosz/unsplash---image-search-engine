d70ee9639262bd382bf265d43347e50b9cdc2300
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-11-05 14:50:29 +0100

    Added styling and text to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,16 +4,28 @@ import './Form.scss';
 function Form( {searchValue, handleSearchValueChange, handleSubmit} ) {
 
   return (
-    <div className='container'>
-      <form className='row' onSubmit={handleSubmit}>
+    <section className='container fullForm'>
+      <header className='row header'>
+        <h1 className='header__title'>Unsplash</h1>
+        <p className='header__text'>
+          The internet's source of <a href="https://unsplash.com/" target='_blank' rel='noreferrer'>freely-usable images</a>.
+        </p>
+        <p className='header__text'>
+          Powered by creators everywhere.
+        </p>
+        <form className='header__form' onSubmit={handleSubmit}>
           <input 
             type='text'
             name='content'
+            className='header__form__input'
+            placeholder='Search free high-resolution photos'
             value={searchValue}
             onChange={e => handleSearchValueChange(e.target.value)}
             />
-      </form>
-    </div>
+        </form>
+        <p className='header__text'>Trending: ..., ..., ..., ...</p>
+      </header>
+    </section>
   );
 }
 
