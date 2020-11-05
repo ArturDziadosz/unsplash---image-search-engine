93d54b273f493b3d1d8fd007049eec444db3a65d
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-11-05 12:55:22 +0100

    Added fetch on query and displaying the result

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Form from './components/Form';
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
+  const [photos, setPhotos] = useState([]);
+  const key = "q8pGKlT0Khz6viOf9A5MXa-3XvXETz3f8xNcchKr9MY";
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(searchValue);
+    fetch(`https://api.unsplash.com/search/photos/?client_id=${key}&query=${searchValue}`)
+    .then(resp => resp.json()
+    .then(data => setPhotos(data.results)))
   }
 
   return (
@@ -18,6 +22,14 @@ function App() {
         handleSearchValueChange={setSearchValue}
         handleSubmit={handleSubmit}
       />
+      <section className='container'>
+        <div className='row' style={{"flexWrap": "wrap"}}>
+          {photos &&
+            photos.map(photo =>
+              <img style={{"width": "300px", "height": "250px"}} key={photo.id} src={photo.urls.full} alt='' />
+          )}
+        </div>
+      </section>
     </div>
   );
 }
