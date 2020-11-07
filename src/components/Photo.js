24d0b9a0eb0d263c4ea2c42b5c951542585e8912
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-11-07 13:26:51 +0100

    Styled big image

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,10 +4,16 @@ import './Photo.scss';
 const Photo = ( {user, src, srcBigger} ) => {
 
   const [isBig, setIsBig] = useState(false);
+  const [clientY, setClientY] = useState(0);
 
-  const handleToggleBigger = () => {
-    setIsBig(!isBig);
-    window.scrollTo(0,0);
+  const handleMakeBigger = e => {
+    setIsBig(true);
+    setClientY(e.pageY);
+    window.scrollTo(0, e.pageY + 50)
+  }
+
+  const handleMakeSmaller = e => {
+    setIsBig(false);
   }
 
   return (
@@ -16,21 +22,31 @@ const Photo = ( {user, src, srcBigger} ) => {
         className='list__elem__img'
         src={src} 
         alt='' 
-        onClick={handleToggleBigger} 
+        onClick={handleMakeBigger} 
       />
       {isBig && 
         <div className='list__elem__container'>
-          <div className='elem__container__user'>
-            <img src={user.profile_image.small} alt='' />
-            <p>{user.name}</p>
-            {user.twitter_username && <p>@{user.twitter_username}</p>}
+          <div className='elem__container__box'  style={{'top': `${clientY}px`}}>
+            <div className='container__box__header'>
+              <div className='box__header__user'>
+                <img src={user.profile_image.small} alt='' />
+                <div>
+                  <p>{user.name}</p>
+                  {user.twitter_username && <p>@{user.twitter_username}</p>}
+                </div>
+              </div>
+              <div className='box__header__delete' onClick={handleMakeSmaller}><i className="fas fa-times"/></div>
+            </div>
+            <img 
+              className='container__box__img'
+              src={srcBigger} 
+              alt='' 
+              onClick={handleMakeSmaller} 
+            />
+            <div className='container__box__footer'>
+              <p>{user.location}</p>
+            </div>
           </div>
-          <img 
-            className='elem__container__img'
-            src={srcBigger} 
-            alt='' 
-            onClick={handleToggleBigger} 
-          />
         </div>
       }
     </li>
